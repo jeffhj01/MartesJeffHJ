3729920a731b6af5a70ab3e198f59d147fd743ba
jefferson hernandez jimenez <jeffhj011198@gmail.com>
2024-02-06 16:25:26 -0600

    Semana 4
    Semana 4

--- ProyectoWeb_Martes/ProyectoWeb_Martes/Controllers/InicioController.cs
+++ ProyectoWeb_Martes/ProyectoWeb_Martes/Controllers/InicioController.cs
@@ -1,27 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using ProyectoWeb_Martes.Entidades;
 using System.Web.Mvc;
 
 namespace ProyectoWeb_Martes.Controllers
 {
     public class InicioController : Controller
     {
-        // GET: Inicio
+        [HttpGet]
         public ActionResult IniciarSesion()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult IniciarSesion(Usuario entidad)
+        {
+            return RedirectToAction("PantallaPrincipal", "Inicio");
+        }
+
+
+        [HttpGet]
         public ActionResult RegistrarUsuario()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult RegistrarUsuario(Usuario entidad)
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult RecuperarAcceso()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RecuperarAcceso(Usuario entidad)
+        {
+            return View();
+        }
+
+
+        [HttpGet]
         public ActionResult PantallaPrincipal()
         {
             return View();
         }
+
     }
 }
